3afe833ac733a1c5031af6cc63c7da2d923a913a
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-14 21:17:41 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,55 @@
-# Write your code here.
+def dictionary
+substitutes = Hash.new
+substitutes = {
+  "hello" => "hi",
+  "to" => "2",
+  "two" => "2",
+  "too" =>"2",
+  "for" => "4",
+  "four" => "3",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&",
+  "For" => "4"
+}
+substitutes
+end
+
+def word_substituter (tweet)
+substitutes = dictionary
+tweet_array = tweet.split(" ")
+tweet_array.each_with_index do |word,index|
+if substitutes.keys.include?(word)
+  tweet_array[index] = substitutes[word]
+else
+end
+end
+tweet_array.join(" ")
+end
+
+def bulk_tweet_shortener (tweet_array)
+  tweet_array.collect do |tweet|
+    puts word_substituter(tweet)
+  end
+  end
+
+def selective_tweet_shortener (tweet)
+  if tweet.length <140
+    tweet
+  else
+    word_substituter(tweet)
+end
+end
+
+def shortened_tweet_truncator (tweet)
+truncated_tweet = String.new
+  if tweet.length<140
+    tweet
+  elsif word_substituter(tweet).length < 140
+    word_substituter(tweet)
+  else
+    truncated_tweet = word_substituter(tweet)[0..136]
+    truncated_tweet << ("...")
+  end
+end
