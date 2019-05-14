7588a1fd08cfb077f9f70e5983da4b1727b2a470
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-14 20:52:26 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def dictionary
+substitutes = Hash.new 
+substitutes = {
+  "hello" => "hi",
+  "to" => "2",
+  "two" => "2",
+  "too" =>"2",
+  "for" => "4",
+  "four" => "3",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+}
+substitutes
+end
+
+def word_substituter (tweet)
+substitutes = dictionary
+tweet_array = tweet.split(" ")
+tweet_array.each_with_index do |word,index|
+if substitutes.keys.include?(word)
+  tweet_array[index] = substitutes[word]
+else
+end
+end
+tweet_array.join(" ")
+end  
+
